f64750676c5ff81ded46ce59dd6668d2d9aff47d
Henry Doan <henrydoan96@gmail.com>
2019-04-10 13:09:23 -0600

    start of ttt

--- guess/js/app.js
+++ guess/js/app.js
@@ -26,18 +26,23 @@ function logic(rightNum) {
 
 function img(result) {
   var image = document.getElementById('image')
+  var msg = document.getElementById('message')
 
   switch(result) {
     case 'win':
+      msg.innerText = 'Winner! Play again?'
       image.src = './img/1850893_2.jpg'
       break
     case 'high':
+      msg.innerText = 'Too High! Guess again'
       image.src = './img/photo-1500577329392-f0af6d7dd827.jpeg'
       break
     case 'low':
+      msg.innerText = 'A Little Low! Try again'
       image.src = './img/photo-1531928351158-2f736078e0a1.jpeg'
       break
     default: 
+      msg.innerText = 'Sorry it looks like you entered a invalid input'
       image.src = './img/photo-1543702883-c93cb11519d3.jpeg'
   }
 }

0423bb906d08095f056f8a80b270f8c9c509805b
Henry Doan <henrydoan96@gmail.com>
2019-04-24 18:31:36 -0600

    finished with class ex from w3school

---
+++ bootstrap-inclass-2/js/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $('[data-toggle="tooltip"]').tooltip(); 
+  $(".navbar a, footer a[href='#myPage']").on('click', function(e) {
+    if (this.hash !== "") {
+      e.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 900, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+})
